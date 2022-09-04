87846b3930b2b7141dca9807734a4bc60da5498b
James Hodgkinson <james@terminaloutcomes.com>
2022-09-05 09:00:30 +1000

    not really a notifications thing but here's me dumping billing data

---
+++ github_notifications_cleaner/billing.py
@@ -0,0 +1,55 @@
+""" playing with billing """
+
+import json
+from typing import Any, Dict
+from github.AuthenticatedUser import AuthenticatedUser
+from . import do_login, Settings
+
+
+def get_billing_actions_for_user(user_object: AuthenticatedUser, username) -> Dict[str, Any]:
+    """
+    :calls: `GET /users/{username}/settings/billing/actions <https://docs.github.com/en/rest/billing#get-github-actions-billing-for-a-user>`_
+    :rtype: Dict
+    """
+    assert isinstance(username, str), username
+    url = f"/users/{username}/settings/billing/actions"
+
+    ####pylint: disable=protected-access
+    _, data = user_object._requester.requestJsonAndCheck("GET", url) # type: ignore
+
+    return data
+
+def get_billing_packages_for_user(user_object: AuthenticatedUser, username) -> Dict[str, Any]:
+    """
+    :calls: `GET /users/{username}/settings/billing/packages <https://docs.github.com/en/rest/billing#get-github-actions-billing-for-a-user>`_
+    :rtype: Dict
+    """
+    assert isinstance(username, str), username
+    url = f"/users/{username}/settings/billing/packages"
+
+    ####pylint: disable=protected-access
+    _, data = user_object._requester.requestJsonAndCheck("GET", url) # type: ignore
+
+    return data
+
+def get_billing_storage_for_user(user_object: AuthenticatedUser, username) -> Dict[str, Any]:
+    """
+    :calls: `GET /users/{username}/settings/billing/shared-storage <https://docs.github.com/en/rest/billing#get-github-actions-billing-for-a-user>`_
+    :rtype: Dict
+    """
+    assert isinstance(username, str), username
+    url = f"/users/{username}/settings/billing/shared-storage"
+
+    ####pylint: disable=protected-access
+    _, data = user_object._requester.requestJsonAndCheck("GET", url) # type: ignore
+
+    return data
+
+gh = do_login(Settings())
+user = gh.get_user()
+billing_data = get_billing_actions_for_user(user, "yaleman")
+print(json.dumps(billing_data))
+billing_data = get_billing_packages_for_user(user, "yaleman")
+print(json.dumps(billing_data))
+billing_data = get_billing_storage_for_user(user, "yaleman")
+print(json.dumps(billing_data))
