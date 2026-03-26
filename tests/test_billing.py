d0237d10b4aa7fb6afc812441084fbce5a230bae
James Hodgkinson <james@terminaloutcomes.com>
2026-03-27 07:26:22 +1000

    stupid github rest APIs

--- tests/test_billing.py
+++ tests/test_billing.py
@@ -7,7 +7,8 @@ import sys
 
 from github.AuthenticatedUser import AuthenticatedUser
 from loguru import logger
-from github_notifications_cleaner.billing import get_billing_actions_for_user
+
+# from github_notifications_cleaner.billing import get_billing_actions_for_user
 from github_notifications_cleaner import Settings, do_login
 
 
